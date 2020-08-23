b4dcc0dbdba4e3196015782c5d979837f364e3f5
BK <brian_j_king@me.com>
2020-08-23 18:59:56 -0500

    part Two is done BJKAug2320

--- MethodExerise/Program.cs
+++ MethodExerise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MethodExerise
 {
@@ -11,6 +12,7 @@ namespace MethodExerise
             //Favorite Color: Blue
             //Favorite Animal: Walrus
             //Favorite Band: The Beatles
+
 
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
@@ -31,7 +33,52 @@ namespace MethodExerise
                              $"{name} was eating {favoriteFood} and wearing a {favoriteColor} t-shirt \n" +
                              $"and rocking out to {favoriteBand}");
 
+            Console.ForegroundColor = ConsoleColor.Green;
+            int x = 2;
+            int y = 3;
+            int[] myArray = { 2, 3, 3, 4, 5 };
 
+
+            Console.WriteLine(add(x,y));
+            Console.WriteLine(multiply(x, y));
+
+            Console.WriteLine(add(myArray));
+            Console.WriteLine(multiply(myArray));
         }
+
+        public static int add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int add(int [] myArray)
+        {
+            var sum = 0;
+
+            foreach (var num in myArray)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+
+
+        public static int multiply(int num1, int num2) 
+        {
+            return num1 * num2;
+        }
+
+        public static int multiply(int [] myArray)
+        {
+            
+            var result = 1;
+
+            foreach (var num in myArray)
+            {
+                result *= num;
+            }
+            return result;
+        }
+
     }
 }
